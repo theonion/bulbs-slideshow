5c50dd0a541830a4f002c441849fbb01da4ff5d0
Cameron Lowe <cam.m.lowe@gmail.com>
2015-11-05 15:13:49 -0600

    basic slide pages controller

--- bulbs-cms/slideshow-edit/slideshow-edit.js
+++ bulbs-cms/slideshow-edit/slideshow-edit.js
@@ -2,7 +2,9 @@
 
 
 angular.module('bulbs.slideshow.edit', [
+
 ])
+
   .directive('slideshowEdit', [
     function () {
       return {
@@ -10,6 +12,82 @@ angular.module('bulbs.slideshow.edit', [
         templateUrl: 'bulbs-cms/slideshow-edit/slideshow-edit.html',
         scope: {
           article: '='
-        }
+        },
+        controller: [
+          '$scope', '$window', '$timeout',
+          function($scope, $window, $timeout) {
+            $scope.isEditing = false;
+            $scope.selectedPage = null;
+            $scope.$watch('article', function (newVal, oldVal) {
+              if ($scope.selectedPage) {
+                $scope.selectedPage = _.find($scope.pages, function (value) {
+                  return value.order == $scope.selectedPage.order;
+                });
+              }
+            });
+            $scope.selectPage = function (page) {
+              $scope.selectedPage = page;
+              $scope.isEditing = !!page;
+              if ($scope.isEditing) {
+                $timeout(function () {
+                    $window.picturefill($('.slideshow-page')[0]);
+                });
+              }
+            };
+            $scope.onAddPage = function () {
+              var page = {
+                name: '',
+                body: '',
+                order: $scope.article.slides.length
+              };
+              $scope.article.slides.push(page);
+              updateOrderIds($scope.article.slides, 'order');
+              $scope.selectPage(page);
+            };
+            $scope.onDeletePage = function (objList, obj) {
+              var idx = objList.indexOf(obj);
+              if (idx >= 0) {
+                objList.splice(idx, 1);
+                updateOrderIds(objList, 'order');
+                if ($scope.selectedPage && obj.order == $scope.selectedPage.order) {
+                    $scope.selectPage(null);
+                }
+              }
+            };
+            $scope.onClonePage = function (obj) {
+              $scope.onAddPage();
+              var selected = $scope.selectedPage;
+              angular.copy(obj, selected);
+              selected.title = 'Clone ' + selected.title;
+            };
+            $scope.onDeleteObject = function (objList, obj) {
+              var idx = objList.indexOf(obj);
+              if (idx >= 0) {
+                objList.splice(idx, 1);
+                updateOrderIds(objList, 'order');
+              }
+            };
+            $scope.onMoveListObject = function(objList, startIndex, newIndex) {
+              if (startIndex >= 0 && newIndex >= 0 && newIndex < objList.length) {
+                var obj = objList[startIndex];
+                objList.splice(startIndex, 1);
+                objList.splice(newIndex, 0, obj);
+                updateOrderIds(objList, 'order');
+              }
+            };
+            function updateOrderIds(objList, fieldName) {
+              for (var i = 0; i < objList.length; i++) {
+                objList[i][fieldName] = i;
+              }
+            }
+
+            if ($scope.article.slides.length === 0) {
+              $scope.onAddPage();
+            }
+
+            updateOrderIds($scope.article.slides, 'order');
+          }
+
+        ],
       };
   }]);
